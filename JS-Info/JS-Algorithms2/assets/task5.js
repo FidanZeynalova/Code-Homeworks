1a31f4e68a245a4aa208336a59e23c8469ca3097
FidanZeynalova <fidanhz-ap206@code.edu.az>
2024-11-04 12:53:21 +0400

    JS-Algorithms2(Task Done)

--- JS-Info/JS-Algorithms2/assets/task5.js
+++ JS-Info/JS-Algorithms2/assets/task5.js
@@ -1,50 +1,26 @@
 // 5.Verilmiş arrayın cüt ədədlərinin maksimumu ilə tək ədədlərinin minimumunun yerini dəyişən proqram tərtib edin. (custom yaz).
 
-// let arr=[23,1,5,3,12,4,9,81,7,11,22,31,6]
-// let cutMax = 0
-// let tekMin = arr[0]
-// for( let i = 0; i < arr.length; i ++){
-//     if (cutMax < arr[i] && arr[i] % 2 == 0) {
-//         cutMax = arr[i]
-//     }
-//     if (tekMin > arr[i] && arr[i] % 2 == 1) {
-//         tekMin = arr[i]
-//     }
-   
-// }
-// let maxResultIndex = arr.indexOf(cutMax)
-// let minResultIndex = arr.indexOf(tekMin)
-// let newSplice = arr.splice(maxResultIndex,1,minResultIndex)
-
-// console.log(cutMax,maxResultIndex);
-// console.log(tekMin,minResultIndex);
-
-
-
-
-let cutMaxIndex = -1;    
-let tekMinIndex = -1;     
-
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 === 0) { 
-        if (arr[i] > cutMax) {
-            cutMax = arr[i];
-            cutMaxIndex = i; 
-        }
-    } else { 
-        if (arr[i] < tekMin) {
-            tekMin = arr[i];
-            tekMinIndex = i; 
-        }
+let arr=[23,1,5,3,12,4,9,81,7,11,22,31,6]
+let cutMax = 0
+let tekMin = arr[0]
+for( let i = 0; i < arr.length; i ++){
+    if (cutMax < arr[i] && arr[i] % 2 == 0) {
+        cutMax = arr[i]
     }
+    if (tekMin > arr[i] && arr[i] % 2 == 1) {
+        tekMin = arr[i]
+    }
+   
 }
-
-if (cutMaxIndex !== -1 && tekMinIndex !== -1) { 
-    let temp = arr[cutMaxIndex];
-    arr[cutMaxIndex] = arr[tekMinIndex];
-    arr[tekMinIndex] = temp;
-}
+let maxResultIndex = arr.indexOf(cutMax)
+let minResultIndex = arr.indexOf(tekMin)
+let newMaxSplice = arr.splice(maxResultIndex,1,tekMin)
+let newMinSplice = arr.splice(minResultIndex,1,cutMax)
 console.log(arr);
+
+
+
+
 
 
 
