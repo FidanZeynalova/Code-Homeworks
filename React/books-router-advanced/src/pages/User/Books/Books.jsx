f8c15cfc9a42443a13643eb9f4f5e64b42956649
FidanZeynalova <fidanhz-ap206@code.edu.az>
2024-12-27 09:35:11 +0400

    advanced-router

--- React/books-router-advanced/src/pages/User/Books/Books.jsx
+++ React/books-router-advanced/src/pages/User/Books/Books.jsx
@@ -1,21 +1,38 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
 function Books() {
+  let[books,setBooks] = useState([])
+
+useEffect(()=>{
+  axios.get("http://localhost:4000/books")
+  .then((res)=>setBooks(res.data)
+  )
+},[])
   return (
     <div className='user-books-container'>
+      <h1>User Books</h1>
       <div className="user-books">
-        <div className="user-book">
-          <div className="image">
-          <img src="https://static.insales-cdn.com/images/products/1/4761/171070105/9963d7a96b3e03dad6271412348121bd.jpg" alt="Səndən Öncə Mən" />
-            <div className="content">
-              <h2>Book Title:Səndən Öncə Mən</h2>
-              <p>Decription:Əlil gəncin həyatına daxil olan bir qadının onun dünyasını necə dəyişdiyini izah edən emosional bir roman.</p>
-              <p>Author:Jojo Moyes</p>
-              <p>Price:20$</p>
-              <p>Language:Azerbaycanca</p>
+        {
+            books.map(book =>(
+              <div className="user-book">
+            <div className="image">
+            <img src={book.image} alt={book.title}/>
+              <div className="content">
+                <h2>Book Title:{book.title}</h2>
+                <p>Description:{book.description.slice(0,40)}... </p>
+                <p>Author:{book.author}</p>
+                <p>Price:{book.price}$</p>
+                <p>Language:{book.language}</p>
+              </div>
+              <div className="btns">
+              <button><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 110c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z"></path></svg></button>
+                <button><svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg></button>
+                <button><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 576 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M253.3 35.1c6.1-11.8 1.5-26.3-10.2-32.4s-26.3-1.5-32.4 10.2L117.6 192 32 192c-17.7 0-32 14.3-32 32s14.3 32 32 32L83.9 463.5C91 492 116.6 512 146 512L430 512c29.4 0 55-20 62.1-48.5L544 256c17.7 0 32-14.3 32-32s-14.3-32-32-32l-85.6 0L365.3 12.9C359.2 1.2 344.7-3.4 332.9 2.7s-16.3 20.6-10.2 32.4L404.3 192l-232.6 0L253.3 35.1zM192 304l0 96c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-96c0-8.8 7.2-16 16-16s16 7.2 16 16zm96-16c8.8 0 16 7.2 16 16l0 96c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-96c0-8.8 7.2-16 16-16zm128 16l0 96c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-96c0-8.8 7.2-16 16-16s16 7.2 16 16z"></path></svg></button>
+              </div>
             </div>
-          </div>
         </div>
+          ))
+        }
       </div>
     </div>
   )
